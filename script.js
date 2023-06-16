96a84e7906a2f80f70f67bacb3962c4c1e001295
larry-y-xu <larryxu416@gmail.com>
2023-06-16 15:34:52 -0700

    now has 3 sizes but cannot be overwritten

--- script.js
+++ script.js
@@ -19,8 +19,14 @@ function changeColor() {
     return color;
 }
 
-createTiles(8);
+
+const smallButton = document.getElementById("small");
+const mediumButton = document.getElementById("medium");
+const largeButton = document.getElementById("large");
 const container = document.querySelector("#container");
+smallButton.addEventListener("click", () => {createTiles(8)});
+mediumButton.addEventListener("click", () => {createTiles(16)});
+largeButton.addEventListener("click", () => {createTiles(24)});
 container.addEventListener("mouseover", event => {
     event.target.style.backgroundColor = changeColor();
 });
