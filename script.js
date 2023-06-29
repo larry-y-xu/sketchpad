d3a6ea2a24865e43dfe2f6ec5ba23aadc1462b8d
larry-y-xu <larryxu416@gmail.com>
2023-06-28 17:21:44 -0700

    supports multiple colors

--- script.js
+++ script.js
@@ -1,22 +1,24 @@
 function createTiles(size) {
     let parent = document.getElementById("container");
-    let tileWidth = parent.offsetWidth / size + "px";
-    for (let i=0; i<size*size; i++) {
-        let newDiv = document.createElement("div");
-        newDiv.className = "tile";
-        newDiv.style.width = tileWidth;
-        newDiv.style.height = tileWidth;
-        parent.appendChild(newDiv);
+    let tileWidth = parent.clientWidth / (size + 1) + "px";
+    for (let i=0; i<size; i++) {
+        for (let j=0; j<size; j++) {
+            let newDiv = document.createElement("div");
+            newDiv.className = "tile";
+            newDiv.style.width = tileWidth;
+            newDiv.style.height = tileWidth;
+            parent.appendChild(newDiv);
+        }
     }
 }
 
-function random(number) {
-    return Math.floor(Math.random()*number);
+function random() {
+    return Math.floor(Math.random()*256);
 }
 
-function changeColor() {
-    const color = `rgb(0, 0, 0)`;
-    return color;
+
+function changeColor(r, g, b) {
+    color = `rgb(${r}, ${g}, ${b})`;
 }
 
 function clear() {
@@ -26,12 +28,21 @@ function clear() {
 const smallButton = document.getElementById("small");
 const mediumButton = document.getElementById("medium");
 const largeButton = document.getElementById("large");
+const redButton = document.getElementById("red");
+const greenButton = document.getElementById("green");
+const blueButton = document.getElementById("blue");
+const blackButton = document.getElementById("black");
 const clearButton = document.getElementById("clear");
 const container = document.querySelector("#container");
-smallButton.addEventListener("click", () => {createTiles(8)});
-mediumButton.addEventListener("click", () => {createTiles(16)});
-largeButton.addEventListener("click", () => {createTiles(24)});
+let color = `rgb(0, 0, 0)`;
+smallButton.addEventListener("click", () => {createTiles(4)});
+mediumButton.addEventListener("click", () => {createTiles(8)});
+largeButton.addEventListener("click", () => {createTiles(12)});
+redButton.addEventListener("click", () => {changeColor(255, 0, 0)});
+greenButton.addEventListener("click", () => {changeColor(0, 255, 0)});
+blueButton.addEventListener("click", () => {changeColor(0, 0, 255)});
+blackButton.addEventListener("click", () => {changeColor(0, 0, 0)});
 clearButton.addEventListener("click", () => {clear()});
-container.addEventListener("mouseover", event => {
-    event.target.style.backgroundColor = changeColor();
+container.addEventListener("click", event => {
+    event.target.style.backgroundColor = color;
 });
